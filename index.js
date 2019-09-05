b8b1150100b993e6609dc357409a09789b4fe0d3
Rankit Ranjan <rankit4u@gmail.com>
2019-09-06 02:47:09 +0530

    fix

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/', (req, res) => {
+app.post('/send_no_reply', (req, res) => {
 	console.log('called');
-  const { spawn } = require('child_process');
-	const child = spawn('ls', );
-	child.stdout.on('data', (chunk) => {
-		console.log(`child process exited with code ${chunk}`);
-		res.send('Hello World!');
-	});
+	const exec = require('child_process').exec;
+    // const child = exec('echo "Hello there" | mail -s "testing" -r no-reply@appbrowzer.com rankit4u@gmail.com');
+    // `child process exited with code ${code}`
+    let { body, subject, from, to } = req.body;
+    if (body && subject && to) {
+	    from =  req.body.from || 'no-reply@appbrowzer.com';
+	    console.log(`echo "${body}" | mail -s "${subject}" -r ${from} ${to}`);
+	    const child = exec(`echo "${body}" | mail -s "${subject}" -r ${from} ${to}`);
+		child.stdout.on('data', (chunk) => {
+			console.log(`op => ${chunk}`);
+			res.send('Done');
+		});
+	} else {
+		res.send('invalid request');
+	}
 });
 
 app.listen(process.env.PORT || 3340, function() {
   console.log('Listening on port 3340');
 });
+
